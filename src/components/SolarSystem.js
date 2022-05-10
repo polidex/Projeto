b39a077e1c8431a32a7b5a6eafe1211744bd4164
polidex <lp.igor@gmail.com>
2022-05-10 10:33:13 -0300

    2 done

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from './components/Title';
+
+
+class SolarSystem extends React.Component { 
+  render() {
+    return (
+      <div data-testid="solar-system">
+        <Title />
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;

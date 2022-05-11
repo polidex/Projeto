e82bcb99421a7eb02b329b6a4e366dc822b2fc2a
polidex <lp.igor@gmail.com>
2022-05-11 20:38:46 -0300

    10 done e projeto finalizado

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -1,13 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MissionCard extends React.Component {
   render() {
-    const {
-      name,
-      year,
-      country,
-      destination,
-    } = this.props;
+    const { name, year, country, destination } = this.props;
     return (
       <div data-testid="mission-card">
         <p data-testid="mission-name">{name}</p>
@@ -15,8 +11,15 @@ class MissionCard extends React.Component {
         <p data-testid="mission-country">{country}</p>
         <p data-testid="mission-destination">{destination}</p>
       </div>
-    )
+    );
   }
 }
+
+MissionCard.propTypes = {
+  name: PropTypes.string.isRequired,
+  year: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  destination: PropTypes.string.isRequired,
+};
 
 export default MissionCard;
